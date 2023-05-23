806879a10e22db1b4f73304533930ecc2a550d64
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-23 17:12:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,67 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 std::string translatePythonToCpp(const std::string& pythonCode) {
     std::string cppCode;
     size_t pos = 0;
 
     while (pos < pythonCode.length()) {
-        size_t end = pythonCode.find('\n', pos);
+        size_t end = pythonCode.find('\n', pos); 
 
         if (end == std::string::npos)
             end = pythonCode.length();
 
-        std::string line = pythonCode.substr(pos, end - pos);
+        std::string line = pythonCode.substr(pos, end - pos); 
 
-        // Variable declaration translation
-        if (line.find(" = ") != std::string::npos) {
-            size_t equalPos = line.find(" = ");
-            std::string variableName = line.substr(0, equalPos);
-            std::string variableValue = line.substr(equalPos + 3);
-            cppCode += "int " + variableName + " = " + variableValue + ";\n";
+        try {
+            if (line.find(" = ") != std::string::npos) {
+                size_t equalPos = line.find(" = ");
+                std::string variableName = line.substr(0, equalPos);
+                std::string variableValue = line.substr(equalPos + 3);
+                cppCode += "int " + variableName + " = " + variableValue + ";\n";
+            }
+            // Print statement translation
+            else if (line.find("print(") != std::string::npos) {
+                size_t openParenPos = line.find('(');
+                size_t closeParenPos = line.find(')');
+                if (openParenPos == std::string::npos || closeParenPos == std::string::npos)
+                    throw std::runtime_error("Invalid print statement syntax");
+                
+                std::string message = line.substr(openParenPos + 1, closeParenPos - openParenPos - 2);
+                cppCode += "std::cout << " + message + " << std::endl;\n";
+            }
+            // If statement translation
+            else if (line.find("if ") != std::string::npos) {
+                size_t colonPos = line.find(':');
+                if (colonPos == std::string::npos)
+                    throw std::runtime_error("Invalid if statement syntax");
+
+                std::string condition = line.substr(3, colonPos - 3);
+                cppCode += "if (" + condition + ") {\n";
+            }
+            // While loop translation
+            else if (line.find("while ") != std::string::npos) {
+                size_t colonPos = line.find(':');
+                if (colonPos == std::string::npos)
+                    throw std::runtime_error("Invalid while loop syntax");
+
+                std::string condition = line.substr(6, colonPos - 6);
+                cppCode += "while (" + condition + ") {\n";
+            }
+            // Arithmetic operation translation
+            else if (line.find('+') != std::string::npos || line.find('-') != std::string::npos ||
+                    line.find('*') != std::string::npos || line.find('/') != std::string::npos) {
+                cppCode += line + ";\n";
+            }
+            // Add more translation rules for other statements as needed
+            else {
+                throw std::runtime_error("Unsupported statement: " + line);
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            // Handle the error as needed
         }
-        // Print statement translation
-        else if (line.find("print(") != std::string::npos) {
-            size_t openParenPos = line.find('(');
-            size_t closeParenPos = line.find(')');
-            std::string message = line.substr(openParenPos + 1, closeParenPos - openParenPos - 2);
-            cppCode += "std::cout << " + message + " << std::endl;\n";
-        }
-        // If statement translation
-        else if (line.find("if ") != std::string::npos) {
-            size_t colonPos = line.find(':');
-            std::string condition = line.substr(3, colonPos - 3);
-            cppCode += "if (" + condition + ") {\n";
-        }
-        // While loop translation
-        else if (line.find("while ") != std::string::npos) {
-            size_t colonPos = line.find(':');
-            std::string condition = line.substr(6, colonPos - 6);
-            cppCode += "while (" + condition + ") {\n";
-        }
-        // Arithmetic operation translation
-        else if (line.find('+') != std::string::npos || line.find('-') != std::string::npos ||
-                 line.find('*') != std::string::npos || line.find('/') != std::string::npos) {
-            cppCode += line + ";\n";
-        }
-        // Add more translation rules for other statements as needed
 
         pos = end + 1;
     }
@@ -66,6 +83,8 @@ int main() {
         while x > 0:
             print(x)
             x -= 1
+
+        invalid statement
     )";
 
     std::string cppCode = translatePythonToCpp(pythonCode);
